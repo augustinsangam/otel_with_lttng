5a7ca200ae8e7d061fc5d1433add7f377a68cd81
Eya-Tom Augustin SANGAM <eya-tom-augustin.sangam@polymtl.ca>
2022-11-27 01:54:22 -0500

    Fix : small changes in CMakeLists.txt

--- src/lttng_span_exporter.cpp
+++ src/lttng_span_exporter.cpp
@@ -19,29 +19,30 @@ opentelemetry::v1::exporter::trace::LttngSpanExporter::
 
 std::unique_ptr<sdk_trace::Recordable> opentelemetry::v1::exporter::trace::
     LttngSpanExporter::MakeRecordable() noexcept {
-  return std::unique_ptr<sdk_trace::Recordable>(
-      new otlp_exporter::OtlpRecordable());
+	return std::unique_ptr<sdk_trace::Recordable>(
+	    new otlp_exporter::OtlpRecordable());
 }
 
 opentelemetry::sdk::common::ExportResult
 opentelemetry::v1::exporter::trace::LttngSpanExporter::Export(
     const opentelemetry::nostd::span<std::unique_ptr<sdk_trace::Recordable>>
         &spans) noexcept {
-  for (auto &recordable_span : spans) {
-    auto span = std::unique_ptr<otlp_exporter::OtlpRecordable>(
-        static_cast<otlp_exporter::OtlpRecordable *>(
-            recordable_span.release()));
-    std::string span_serialized = span->span().SerializeAsString();
-    lttng_ust_tracepoint(otel_with_lttng, otel_span,
-                         span_serialized.c_str());
-    std::string span_debug = span->span().DebugString();
-    lttng_ust_tracepoint(otel_with_lttng, otel_debug_span,
-                         span_debug.c_str());
-  }
-  return opentelemetry::sdk::common::ExportResult::kSuccess;
+	for (auto &recordable_span : spans) {
+		auto span = std::unique_ptr<otlp_exporter::OtlpRecordable>(
+		    static_cast<otlp_exporter::OtlpRecordable *>(
+		        recordable_span.release()));
+		std::string span_serialized = span->span().SerializeAsString();
+		lttng_ust_tracepoint(otel_with_lttng, otel_span,
+		                     span_serialized.c_str());
+		// DEBUG
+		// std::string span_debug = span->span().DebugString();
+		// lttng_ust_tracepoint(otel_with_lttng, otel_debug_span,
+		//                      span_debug.c_str());
+	}
+	return opentelemetry::sdk::common::ExportResult::kSuccess;
 }
 
 bool opentelemetry::v1::exporter::trace::LttngSpanExporter::Shutdown(
     std::chrono::microseconds timeout) noexcept {
-  return true;
+	return true;
 }
